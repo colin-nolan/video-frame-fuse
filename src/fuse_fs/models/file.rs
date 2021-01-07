7878a42a30961cab12213dc5b6dab14e631cf06c
Colin Nolan <cn580@alumni.york.ac.uk>
2021-01-07 01:12:16 +0000

    Work on improving tests.

--- src/fuse_fs/models/file.rs
+++ src/fuse_fs/models/file.rs
@@ -32,7 +32,6 @@ impl FileInformation {
         listed: bool,
         executable: bool,
         writable: bool,
-        // TODO: does this need to be boxed?
         on_data_change: Option<Box<dyn Fn(&str) -> Result<(), String>>>,
     ) -> Self {
         FileInformation {
@@ -47,6 +46,11 @@ impl FileInformation {
     }
 
     pub fn set_data(&mut self, data: Vec<u8>) -> Result<(), String> {
+        if self.data_fetcher.is_some() {
+            return Err(
+                "Cannot set data as it is dynamically generated with a data fetcher".to_string(),
+            );
+        }
         let parsed = match String::from_utf8(data.clone()) {
             Ok(x) => x,
             Err(e) => return Err(e.to_string()),
@@ -63,5 +67,89 @@ impl FileInformation {
             None => self.data.as_ref().unwrap().clone(),
             Some(x) => x(),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::sync::mpsc::channel;
+
+    #[test]
+    fn get_data_when_stored() {
+        let data = "DATA".as_bytes().to_vec();
+
+        let file_information = FileInformation {
+            name: "".to_string(),
+            data_fetcher: None,
+            data: Some(data.clone()),
+            listed: false,
+            executable: false,
+            writable: false,
+            on_data_change: None,
+        };
+        assert_eq!(file_information.get_data(), data);
+    }
+
+    #[test]
+    fn get_data_when_fetched() {
+        let fetcher = Box::new(|| "DATA".as_bytes().to_vec());
+        let expected = fetcher();
+
+        let file_information = FileInformation {
+            name: "".to_string(),
+            data_fetcher: Some(fetcher),
+            data: None,
+            listed: false,
+            executable: false,
+            writable: false,
+            on_data_change: None,
+        };
+        assert_eq!(file_information.get_data(), expected);
+    }
+
+    #[test]
+    fn set_data_when_stored() {
+        let (sender, receiver) = channel();
+        let expected_data = "hello 123";
+
+        let mut file_information = FileInformation {
+            name: "".to_string(),
+            data_fetcher: None,
+            data: None,
+            listed: false,
+            executable: false,
+            writable: false,
+            on_data_change: Some(Box::new(move |received_data| {
+                assert_eq!(received_data, expected_data);
+                sender.send(()).unwrap();
+                Ok(())
+            })),
+        };
+        file_information
+            .set_data(expected_data.as_bytes().to_vec())
+            .unwrap();
+        receiver.recv().unwrap();
+
+        assert_eq!(
+            std::str::from_utf8(file_information.get_data().as_slice()).unwrap(),
+            expected_data
+        );
+    }
+
+    #[test]
+    fn set_data_when_fetched() {
+        let mut file_information = FileInformation {
+            name: "".to_string(),
+            data_fetcher: Some(Box::new(|| "DATA".as_bytes().to_vec())),
+            data: None,
+            listed: false,
+            executable: false,
+            writable: false,
+            on_data_change: None,
+        };
+        assert!(file_information
+            .set_data("other".as_bytes().to_vec())
+            .is_err());
     }
 }
--- src/fuse_fs/models/manifest.rs
+++ src/fuse_fs/models/manifest.rs
@@ -26,3 +26,49 @@ impl DirectoryManifest {
         return writer.into_inner().unwrap();
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use itertools::Itertools;
+    use std::io::Cursor;
+
+    #[test]
+    fn output_when_none_added() {
+        let manifest = DirectoryManifest::new();
+
+        let serialised = manifest.to_vec();
+        let mut reader = csv::Reader::from_reader(Cursor::new(&serialised));
+        assert_eq!(
+            &reader.headers().unwrap().iter().collect_vec(),
+            DIRECTORY_MANIFEST_HEADER
+        );
+    }
+
+    #[test]
+    fn output_when_added() {
+        let mut manifest = DirectoryManifest::new();
+        manifest.add(ImageType::JPG, "1");
+        manifest.add(ImageType::PNG, "2");
+
+        let serialised = manifest.to_vec();
+        let mut reader = csv::Reader::from_reader(Cursor::new(&serialised));
+        assert_eq!(
+            &reader.headers().unwrap().iter().collect_vec(),
+            DIRECTORY_MANIFEST_HEADER
+        );
+
+        assert_eq!(
+            reader
+                .records()
+                .map(|x| x.unwrap().iter().map(str::to_string).collect_vec())
+                .collect_vec()
+                .sort(),
+            vec![
+                format!("{},1", ImageType::JPG.to_string()),
+                format!("{},2", ImageType::PNG.to_string())
+            ]
+            .sort()
+        );
+    }
+}
